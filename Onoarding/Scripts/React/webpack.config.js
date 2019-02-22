e354aad7c60ccf4c1fe878214ab68d74fd3a2080
krish-nair <krish.nair101@gmail.com>
2019-02-22 13:22:03 +1300

    connected to the database

--- Onoarding/Scripts/React/webpack.config.js
+++ Onoarding/Scripts/React/webpack.config.js
@@ -8,15 +8,17 @@
     },
     watch: true,
     module: {
-        rules: [{
-            test: /\.jsx?$/,
-            exclude: /(node_modules)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['babel-preset-env', 'babel-preset-react']
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['babel-preset-env', 'babel-preset-react']
+                    }
                 }
             }
-        }]
+        ]
     }
-}
+};
